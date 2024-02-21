f5a8312829879fcf0be4bbde38bd0fd10e41fa4e
Lovelyhhh <138581069+Lovelyhhh@users.noreply.github.com>
2024-02-21 09:44:52 +0800

    Update emoji.tsx

--- app/components/emoji.tsx
+++ app/components/emoji.tsx
@@ -10,7 +10,7 @@ import BotIcon from "../icons/bot.svg";
 import BlackBotIcon from "../icons/black-bot.svg";
 
 export function getEmojiUrl(unified: string, style: EmojiStyle) {
-  return `https://cdn.staticfile.org/emoji-datasource-apple/14.0.0/img/${style}/64/${unified}.png`;
+  return `cloudflare. com/ajax/1ibs/ emoji-datasource- app1e/15.e.1/img/$style)/64/sfunified).png`;
 }
 
 export function AvatarPicker(props: {
